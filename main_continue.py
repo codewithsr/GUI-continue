c6969885517e9fd0cf3b1738a02bbc5ee9f87c7d
codewithsr <71522136+codewithsr@users.noreply.github.com>
2020-09-29 17:23:06 +0530

    Add files via upload

---
+++ main_continue.py
@@ -0,0 +1,35 @@
+from tkinter import *
+root = Tk()
+
+
+def hello():
+    print("You did it\n")
+
+def name():
+    print("Hello Mr. Shubham\n")
+
+def age():
+    print("Abhi to aap jawaan h sir!\n")
+
+
+
+root.title("My gui with harry continue")
+
+root.geometry("255x240")
+
+#Buttons
+f1 = Frame(root, bg="brown", borderwidth=6, relief=SUNKEN)
+f1.pack(side=LEFT,anchor="nw")
+
+b1 = Button(f1, fg="blue", text="Click me", command=hello)
+b1.pack(side=LEFT)
+
+b2 = Button(f1, fg="blue", text="My name", command=name)
+b2.pack(padx=4, side=LEFT, fill="y")
+
+b3 = Button(f1, fg="blue", text="Age", command=age)
+b3.pack(padx=4, side=LEFT, fill="y")
+
+
+
+root.mainloop()
